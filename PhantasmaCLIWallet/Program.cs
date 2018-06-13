369144a51a515d3a92205e972384763f95e9910d
Relfos <relfos@gmail.com>
2018-06-13 01:54:50 +0100

    Updated cli-wallet to latest chain code

--- PhantasmaCLIWallet/Program.cs
+++ PhantasmaCLIWallet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Phantasma.Core;
 using Phantasma.Cryptography;
+using Phantasma.Network.Kademlia;
 using Phantasma.Utils;
 using System;
 using System.Collections.Generic;
@@ -45,17 +46,29 @@ namespace PhantasmaCLIWallet
 
         static void Main(string[] args)
         {
+            var bytes = "540350540350540350".HexToBytes();
+
+            //IProtocol protocol = new TcpSubnetProtocol("http://127.0.0.1", 2720, n);
+            /*var protocol = new VirtualProtocol();
+
+            DHT dht = new DHT(ID.RandomID, protocol, () => new VirtualStorage(), new Router());
+            
+
+            //server.RegisterProtocol(n, dht.Node);
+            ((VirtualProtocol)protocol).Node = dht.Node;*/
+
+
             Console.WriteLine("Initializng chain...");
 
             var owner = KeyPair.Random();
             Console.WriteLine("Genesis Address: " + owner.address);
 
-            var chain = new Chain(owner, x => Console.WriteLine(x));
+            var chain = new Chain(owner);
 
             var miner = KeyPair.Random();
             var third = KeyPair.Random();
 
-            var tx = new Transaction(owner.PublicKey, ScriptUtils.TransferScript(chain.NativeToken.Symbol, owner.PublicKey, third.PublicKey, 5), 0, 0);
+            var tx = new Transaction(owner.PublicKey, ScriptUtils.TransferScript(chain.NativeTokenPubKey, owner.PublicKey, third.PublicKey, 5), 0, 0);
             tx.Sign(owner);
 
             var nextHeight = chain.lastBlock.Height + 1;
