91605aefb6d708677bd196c123fbd1cd9aa78e44
Relfos <relfos@gmail.com>
2018-06-13 01:54:12 +0100

    Updated solution to VS2017 format

--- PhantasmaChain/Properties/AssemblyInfo.cs
+++ PhantasmaChain/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PhantasmaChain")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Blockchain with native storage and smart contract integration.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("phantasma.io")]
 [assembly: AssemblyProduct("PhantasmaChain")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
