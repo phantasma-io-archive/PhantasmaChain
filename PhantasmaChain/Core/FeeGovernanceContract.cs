a441ccda0fa23bcfe0327d7cc50584612270f491
Relfos <relfiroth@gmail.com>
2018-07-05 00:02:35 +0100

    Added Fee Governance Contract

---
+++ PhantasmaChain/Core/FeeGovernanceContract.cs
@@ -0,0 +1,33 @@
+﻿using Phantasma.VM;
+using System.Numerics;
+
+namespace Phantasma.Core
+{
+    public class FeeGovernanceContract : Contract
+    {
+        public static readonly byte[] DefaultScript = new byte[] { (byte)Opcode.RET };
+        public static readonly byte[] DefaultABI = new byte[] { };
+
+        private byte[] _publicKey;
+        public override byte[] PublicKey => _publicKey;
+
+        public override byte[] Script => DefaultScript;
+        public override byte[] ABI => DefaultABI;
+
+        public BigInteger FeeMultiplier = 1;
+
+        public FeeGovernanceContract(Chain chain, byte[] publicKey) : base(chain)
+        {
+            this._publicKey = publicKey;
+        }
+
+        public void InitVotingRound()
+        {
+        }
+
+        public void Vote(BigInteger value)
+        {
+        }
+
+    }
+}
