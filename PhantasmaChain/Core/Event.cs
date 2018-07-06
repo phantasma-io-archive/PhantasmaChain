f50d5757f8da7924ef4b9393288f28f8e8ab1a10
Relfos <relfiroth@gmail.com>
2018-07-06 16:27:04 +0100

    Added Event serialization

--- PhantasmaChain/Core/Event.cs
+++ PhantasmaChain/Core/Event.cs
@@ -16,18 +16,28 @@ namespace Phantasma.Core
     {
         public readonly EventKind Kind;
         public readonly byte[] PublicKey;
+        public readonly byte[] Data;
 
-        public Event(EventKind kind, byte[] publicKey)
+        public Event(EventKind kind, byte[] publicKey, byte[] data)
         {
             this.Kind = kind;
             this.PublicKey = publicKey;
+            this.Data = data;
         }
 
         public void Serialize(BinaryWriter writer)
         {
             writer.Write((byte)this.Kind);
             writer.WriteByteArray(this.PublicKey);
-            //writer.WriteByteArray(this.Data);
+            writer.WriteByteArray(this.Data);
+        }
+
+        internal static Event Unserialize(BinaryReader reader)
+        {
+            var kind = (EventKind)reader.ReadByte();
+            var pubKey = reader.ReadByteArray();
+            var data = reader.ReadByteArray();
+            return new Event(kind, pubKey, data);
         }
     }
 }
