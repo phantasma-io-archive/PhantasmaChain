92bccf7661ee12b997eed344cfebac85fffe1ec7
Sergio Flores <relfos@gmail.com>
2018-08-18 20:22:44 +0100

    Added SHA3Keccak implementation

---
+++ PhantasmaChain/Cryptography/Hashing/SHA3Keccak.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phantasma.Cryptography.Hashing
+{
+    public static class SHA3Keccak
+    {
+        public static byte[] CalculateHash(byte[] value)
+        {
+            var digest = new KeccakDigest(256);
+            var output = new byte[digest.GetDigestSize()];
+            digest.BlockUpdate(value, 0, value.Length);
+            digest.DoFinal(output, 0);
+            return output;
+        }
+    }
+
+}
