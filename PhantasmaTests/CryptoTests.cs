86e7a96e6e07906efe58b2426ee33735698a0b2b
Sergio Flores <relfos@gmail.com>
2018-08-19 15:34:28 +0100

    More tests for Cryptography classes

--- PhantasmaTests/CryptoTests.cs
+++ PhantasmaTests/CryptoTests.cs
@@ -3,12 +3,38 @@ using System.Text;
 using Phantasma.Cryptography;
 using Phantasma.Utils;
 using Phantasma.VM.Types;
+using System;
+using System.Linq;
+using System.Numerics;
 
 namespace PhantasmaTests
 {
     [TestFixture]
     public class CryptoTests
     {
+        [Test]
+        public void HashTests()
+        {
+            var bytes = new byte[32];
+            var rnd = new Random();
+            rnd.NextBytes(bytes);
+
+            var hash = new Hash(bytes);
+
+            Assert.IsTrue(hash.ToByteArray().Length == 32);
+            Assert.IsTrue(hash.ToByteArray().SequenceEqual(bytes));
+
+            bytes = new byte[10];
+            rnd.NextBytes(bytes);
+
+            var number = new BigInteger(bytes);
+            hash = number;
+            Assert.IsTrue(hash.ToByteArray().Length == 32);
+
+            BigInteger other = hash;
+            Assert.IsTrue(number == other);
+        }
+
         [Test]
         public void KeyPairSign()
         {
@@ -23,6 +49,12 @@ namespace PhantasmaTests
 
             var verified = signature.Verify(msgBytes, keys.Address);
             Assert.IsTrue(verified);
+
+            // make sure that Verify fails for other addresses
+            var otherKeys = KeyPair.Generate();
+            Assert.IsFalse(otherKeys.Address == keys.Address);
+            verified = signature.Verify(msgBytes, otherKeys.Address);
+            Assert.IsFalse(verified);
         }
     }
 }
