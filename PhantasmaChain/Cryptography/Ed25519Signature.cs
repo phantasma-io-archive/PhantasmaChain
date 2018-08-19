92b50f5a57fdb06629eca2b8958c8711f4a1b441
Sergio Flores <relfos@gmail.com>
2018-08-19 13:24:51 +0100

    Ed25519 Signature implementation

---
+++ PhantasmaChain/Cryptography/Ed25519Signature.cs
@@ -0,0 +1,20 @@
+﻿using Phantasma.VM.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phantasma.Cryptography
+{
+    public class Ed25519Signature : Signature
+    {
+        public Ed25519Signature(byte[] bytes): base(bytes)
+        {
+
+        }
+
+        public override bool Verify(byte[] message, Address address)
+        {
+            return Ed25519.Verify(this.bytes, message, address.PublicKey);
+        }
+    }
+}
