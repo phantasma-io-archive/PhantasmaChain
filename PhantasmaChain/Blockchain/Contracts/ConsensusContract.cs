3251f019b71cc0695d70a24ca453c7861b5c6484
Sergio Flores <relfos@gmail.com>
2018-08-25 16:00:41 +0100

    Added Consensus contract

---
+++ PhantasmaChain/Blockchain/Contracts/ConsensusContract.cs
@@ -0,0 +1,26 @@
+﻿using Phantasma.Cryptography;
+using Phantasma.Mathematics;
+using System;
+using System.Collections.Generic;
+
+namespace Phantasma.Blockchain.Contracts
+{
+    public sealed class ConsensusContract : NativeContract
+    {
+        internal override NativeContractKind Kind => NativeContractKind.Consensus;
+
+        public ConsensusContract() : base()
+        {
+        }
+
+        public bool IsValidMiner(Address address)
+        {
+            return true;
+        }
+
+        public bool IsValidReceiver(Address address)
+        {
+            return true;
+        }
+    }
+}
