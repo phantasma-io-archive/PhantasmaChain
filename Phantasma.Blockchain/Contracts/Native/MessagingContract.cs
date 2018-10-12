096685a9c7490ba23c0e7a7ca9e91361b402499c
Sergio Flores <relfos@gmail.com>
2018-10-12 17:45:24 +0100

    Added Messaging contract

---
+++ Phantasma.Blockchain/Contracts/Native/MessagingContract.cs
@@ -0,0 +1,14 @@
+﻿using Phantasma.Cryptography;
+using Phantasma.Numerics;
+
+namespace Phantasma.Blockchain.Contracts.Native
+{
+    public sealed class MessagingContract : NativeContract
+    {
+        internal override ContractKind Kind => ContractKind.Naming;
+
+        public MessagingContract() : base()
+        {
+        }
+    }
+}
