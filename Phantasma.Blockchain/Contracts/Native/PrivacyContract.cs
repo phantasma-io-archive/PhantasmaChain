0069acbb9f3a95a812a91503fd4aee59637559db
Sergio Flores <relfos@gmail.com>
2018-10-12 19:38:31 +0100

    Privacy send initial implementation

--- Phantasma.Blockchain/Contracts/Native/PrivacyContract.cs
+++ Phantasma.Blockchain/Contracts/Native/PrivacyContract.cs
@@ -1,29 +1,76 @@
 ﻿using Phantasma.Cryptography;
+using Phantasma.Cryptography.Ring;
 using Phantasma.Numerics;
+using System.Collections.Generic;
 
 namespace Phantasma.Blockchain.Contracts.Native
 {
+    internal struct PrivacyQueue
+    {
+        public uint ID;
+        public string symbol;
+        public List<Address> addresses;
+        public List<RingSignature> signatures;
+    }
+
     public sealed class PrivacyContract : NativeContract
     {
         internal override ContractKind Kind => ContractKind.Privacy;
 
-        public const uint TransferAmount = 10;
+        public static readonly BigInteger TransferAmount = 10;
+
+        private PrivacyQueue _queue;
 
         public PrivacyContract() : base()
         {
+            _queue = new PrivacyQueue() { addresses = new List<Address>(), signatures = new List<RingSignature>(),  symbol = "SOUL", ID = 333 };
         }
 
-        public void SendPrivate(Address from, string symbol)
+        public uint PutPrivate(Address from, string symbol)
         {
             Expect(IsWitness(from));
 
             var token = this.Nexus.FindTokenBySymbol(symbol);
             Expect(token != null);
 
-            var balance = this.Chain.GetTokenBalance(token, from);
+            var balances = this.Chain.GetTokenBalances(token);
+            var balance = balances.Get(from);
             Expect(balance >= TransferAmount);
 
+            var queue = _queue;
+            foreach (var address in queue.addresses)
+            {
+                Expect(address != from);
+            }
 
+            balances.Subtract(from, TransferAmount);
+            queue.addresses.Add(from);
+
+            return queue.ID; // TODO should be quueue ID
+        }
+
+        public void TakePrivate(Address to, string symbol, uint queueID, RingSignature signature)
+        {
+            var queue = _queue;
+
+            Expect(queue.symbol == symbol);
+            Expect(queue.ID == queueID);
+
+            var msg = this.Transaction.ToByteArray(false);
+            Expect(signature.Verify(msg, queue.addresses));
+
+            var token = this.Nexus.FindTokenBySymbol(symbol);
+            Expect(token != null);
+
+            foreach (var otherSignature in queue.signatures)
+            {
+                Expect(!signature.IsLinked(otherSignature));
+            }
+
+            queue.signatures.Add(signature);
+
+            var balances = this.Chain.GetTokenBalances(token);
+            balances.Add(to, TransferAmount);
         }
     }
 }
