2f9c7a0431b5c4d8c6f1f504338398ceba351fbd
Sergio Flores <relfos@gmail.com>
2018-10-15 20:51:37 +0100

    Added TokenUtils

---
+++ Phantasma.Blockchain/TokenUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Phantasma.Numerics;
+
+namespace Phantasma.Blockchain
+{
+    public static class TokenUtils
+    {
+        private const decimal units = 100000000m;
+
+        public static decimal ToDecimal(BigInteger value)
+        {
+            if (value == null || value == 0)
+            {
+                return 0;
+            }
+
+            var n = (long)value;
+            return n / units;
+        }
+
+        public static BigInteger ToBigInteger(decimal n)
+        {
+            var l = (long)(n * units);
+            return new BigInteger(l);
+        }
+    }
+}
