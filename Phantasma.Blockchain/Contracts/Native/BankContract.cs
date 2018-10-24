33b4dcd0e4d0d9205827a083e911d940b9f87b21
Sergio Flores <relfos@gmail.com>
2018-10-24 17:12:27 +0100

    Stable token initial implementation

--- Phantasma.Blockchain/Contracts/Native/BankContract.cs
+++ Phantasma.Blockchain/Contracts/Native/BankContract.cs
@@ -1,7 +1,5 @@
 ﻿using Phantasma.Cryptography;
 using Phantasma.Numerics;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Phantasma.Blockchain.Contracts.Native
 {
@@ -9,5 +7,57 @@ namespace Phantasma.Blockchain.Contracts.Native
     {
         internal override ContractKind Kind => ContractKind.Bank;
 
+        public BigInteger GetRate(string symbol)
+        {
+            Expect(symbol == Nexus.NativeTokenSymbol);
+
+            return TokenUtils.ToBigInteger(0.08m);
+        }
+
+        // SOUL => stable
+        public void Claim(Address target, BigInteger amount)
+        {
+            Expect(IsWitness(target));
+
+            var nativeToken = Runtime.Nexus.FindTokenBySymbol(Nexus.NativeTokenSymbol);
+            Expect(nativeToken != null);
+
+            var stableToken = Runtime.Nexus.FindTokenBySymbol(Nexus.StableTokenName);
+            Expect(stableToken != null);
+
+            var nativeBalances = Runtime.Chain.GetTokenBalances(nativeToken);
+            Expect(nativeToken.Transfer(nativeBalances, target, Runtime.Chain.Address, amount));
+
+            var stableAmount = amount * GetRate(Nexus.NativeTokenSymbol);
+
+            var stableBalances = Runtime.Chain.GetTokenBalances(stableToken);
+            Expect(nativeToken.Mint(stableBalances, target, stableAmount));
+
+            Runtime.Notify(EventKind.TokenSend, target, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, amount = amount, symbol = Nexus.NativeTokenSymbol });
+            Runtime.Notify(EventKind.TokenMint, target, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, amount = stableAmount, symbol = Nexus.StableTokenSymbol });
+        }
+
+        // stable => SOUL
+        public void Redeem(Address target, BigInteger amount)
+        {
+            Expect(IsWitness(target));
+
+            var nativeToken = Runtime.Nexus.FindTokenBySymbol(Nexus.NativeTokenSymbol);
+            Expect(nativeToken != null);
+
+            var stableToken = Runtime.Nexus.FindTokenBySymbol(Nexus.StableTokenName);
+            Expect(stableToken != null);
+
+            var stableBalances = Runtime.Chain.GetTokenBalances(stableToken);
+            Expect(stableToken.Burn(stableBalances, target, amount));
+
+            var expectedAmount = amount / GetRate(Nexus.NativeTokenSymbol);
+
+            var nativeBalances = Runtime.Chain.GetTokenBalances(nativeToken);
+            Expect(nativeToken.Transfer(stableBalances, Runtime.Chain.Address, target, expectedAmount));
+
+            Runtime.Notify(EventKind.TokenReceive, target, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, amount = expectedAmount, symbol = Nexus.NativeTokenSymbol });
+            Runtime.Notify(EventKind.TokenBurn, target, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, amount = amount, symbol = Nexus.StableTokenSymbol });
+        }
     }
 }
