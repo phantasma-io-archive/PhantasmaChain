e5dbba39276adc1415d4d07bcf51d65121926335
Sergio Flores <relfos@gmail.com>
2018-10-26 15:45:34 +0100

    Implicit name => address lookup in VM

--- Phantasma.Blockchain/Contracts/NativeExecutionContext.cs
+++ Phantasma.Blockchain/Contracts/NativeExecutionContext.cs
@@ -26,7 +26,19 @@ namespace Phantasma.Blockchain.Contracts
             var args = new object[method.parameters.Length];
             for (int i=0; i<args.Length; i++) {
                 var arg = stack.Pop();
-                args[i] = arg.Data;
+
+                var temp = arg.Data;
+
+                // when a string is passed instead of an address we do an automatic lookup and replace
+                if (method.parameters[i] == VMType.Object && temp is string)
+                {
+                    var name = (string)temp;
+                    var runtime = (RuntimeVM)frame.VM;
+                    var address = runtime.Nexus.LookUpName(name);
+                    temp = address;
+                }
+
+                args[i] = temp;
             }
 
             var result = this.Contract.CallMethod(methodName, args);
