fe1dc6b45bc4d09db6f530345c4b24d21f7b6fc7
Sergio Flores <relfos@gmail.com>
2018-11-16 16:32:42 +0000

    Added supply verification for side-chain

--- Phantasma.Blockchain/Contracts/Native/NexusContract.cs
+++ Phantasma.Blockchain/Contracts/Native/NexusContract.cs
@@ -38,6 +38,11 @@ namespace Phantasma.Blockchain.Contracts.Native
             var token = this.Runtime.Nexus.CreateToken(Runtime.Chain, owner, symbol, name, maxSupply, (int)decimals, flags);
             Runtime.Expect(token != null, "invalid token");
 
+            if (token.IsCapped)
+            {
+                Runtime.Chain.InitSupplySheet(token, maxSupply);
+            }
+
             Runtime.Notify(EventKind.TokenCreate, owner, symbol);
 
             return token;
@@ -63,146 +68,5 @@ namespace Phantasma.Blockchain.Contracts.Native
 
             return chain;
         }
-
-        #region FUNGIBLE TOKENS
-        public void MintTokens(Address target, string symbol, BigInteger amount)
-        {
-            Runtime.Expect(amount > 0, "amount must be positive and greater than zero");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(token.Flags.HasFlag(TokenFlags.Fungible), "token must be fungible");
-
-            Runtime.Expect(IsWitness(token.Owner), "invalid witness");
-
-            var balances = this.Runtime.Chain.GetTokenBalances(token);
-            Runtime.Expect(token.Mint(balances, target, amount), "minting failed");
-
-            Runtime.Notify(EventKind.TokenMint, target, new TokenEventData() { symbol = symbol, value = amount, chainAddress = this.Runtime.Chain.Address });
-        }
-
-        public void BurnTokens(Address from, string symbol, BigInteger amount)
-        {           
-            Runtime.Expect(amount > 0, "amount must be positive and greater than zero");
-            Runtime.Expect(IsWitness(from), "invalid witness");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(token.Flags.HasFlag(TokenFlags.Fungible), "token must be fungible");
-
-            var balances = this.Runtime.Chain.GetTokenBalances(token);
-            Runtime.Expect(token.Burn(balances, from, amount), "burning failed");
-
-            Runtime.Notify(EventKind.TokenBurn, from, new TokenEventData() { symbol = symbol, value = amount });
-        }
-
-        public void TransferTokens(Address source, Address destination, string symbol, BigInteger amount)
-        {
-            Runtime.Expect(amount > 0, "amount must be positive and greater than zero");
-            Runtime.Expect(source != destination, "source and destination must be different");
-            Runtime.Expect(IsWitness(source), "invalid witness");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(token.Flags.HasFlag(TokenFlags.Fungible), "token must be fungible");
-            Runtime.Expect(token.Flags.HasFlag(TokenFlags.Transferable), "token must be transferable");
-
-            var balances = this.Runtime.Chain.GetTokenBalances(token);
-            Runtime.Expect(token.Transfer(balances, source, destination, amount), "transfer failed");
-
-            Runtime.Notify(EventKind.TokenSend, source, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, value = amount, symbol = symbol });
-            Runtime.Notify(EventKind.TokenReceive, destination, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, value = amount, symbol = symbol });
-        }
-
-        public BigInteger GetBalance(Address address, string symbol)
-        {
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(token.Flags.HasFlag(TokenFlags.Fungible), "token must be fungible");
-
-            var balances = this.Runtime.Chain.GetTokenBalances(token);
-            return balances.Get(address);
-        }
-        #endregion
-
-        #region NON FUNGIBLE TOKENS
-        public BigInteger[] GetTokens(Address address, string symbol)
-        {
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(!token.IsFungible, "token must be non-fungible");
-
-            var ownerships = this.Runtime.Chain.GetTokenOwnerships(token);
-            return ownerships.Get(address).ToArray();
-        }
-
-        public BigInteger MintToken(Address from, string symbol, byte[] data)
-        {
-            Runtime.Expect(IsWitness(from), "invalid witness");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(!token.IsFungible, "token must be non-fungible");
-
-            var tokenID = this.Runtime.Chain.CreateNFT(token, data);
-            Runtime.Expect(tokenID > 0, "invalid tokenID");
-
-            var ownerships = this.Runtime.Chain.GetTokenOwnerships(token);
-            Runtime.Expect(ownerships.Give(from, tokenID), "give token failed");
-
-            Runtime.Notify(EventKind.TokenMint, from, new TokenEventData() { symbol = symbol, value = tokenID });
-            return tokenID;
-        }
-
-        public void BurnToken(Address from, string symbol, BigInteger tokenID)
-        {
-            Runtime.Expect(IsWitness(from), "invalid witness");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(!token.IsFungible, "token must be non-fungible");
-
-            var ownerships = this.Runtime.Chain.GetTokenOwnerships(token);
-            Runtime.Expect(ownerships.Take(from, tokenID), "take token failed");
-
-            Runtime.Expect(this.Runtime.Chain.DestroyNFT(token, tokenID), "destroy token failed");
-
-            Runtime.Notify(EventKind.TokenBurn, from, new TokenEventData() { symbol = symbol, value = tokenID});
-        }
-
-        public void TransferToken(Address source, Address destination, string symbol, BigInteger tokenID)
-        {
-            Runtime.Expect(IsWitness(source), "invalid witness");
-
-            Runtime.Expect(source != destination, "source and destination must be different");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(!token.IsFungible, "token must be non-fungible");
-
-            var ownerships = this.Runtime.Chain.GetTokenOwnerships(token);
-            Runtime.Expect(ownerships.Take(source, tokenID), "take token failed");
-            Runtime.Expect(ownerships.Give(destination, tokenID), "give token failed");
-
-            Runtime.Notify(EventKind.TokenSend, source, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, value = tokenID, symbol = symbol });
-            Runtime.Notify(EventKind.TokenReceive, destination, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, value = tokenID, symbol = symbol });
-        }
-
-        public void SetTokenViewer(Address source, string symbol, string url)
-        {
-            Runtime.Expect(IsWitness(source), "invalid witness");
-
-            var token = this.Runtime.Nexus.FindTokenBySymbol(symbol);
-            Runtime.Expect(token != null, "invalid token");
-            Runtime.Expect(!token.IsFungible, "token must be non-fungible");
-
-            Runtime.Expect(token.Owner == source, "owner expected");
-
-            token.SetViewer(url);
-
-            Runtime.Notify(EventKind.TokenInfo, source, url);
-        }
-
-        #endregion
     }
 }
---
+++ Phantasma.Blockchain/Tokens/SupplySheet.cs
@@ -0,0 +1,129 @@
+﻿using Phantasma.Core;
+using Phantasma.Cryptography;
+using Phantasma.Numerics;
+using System.Collections.Generic;
+
+namespace Phantasma.Blockchain.Tokens
+{
+    public class SupplySheet
+    {
+        public BigInteger ParentBalance { get; private set; }
+        public BigInteger LocalBalance { get; private set; }
+
+        private BigInteger _maxBalance;
+
+        private Dictionary<Address, BigInteger> _childBalances = new Dictionary<Address, BigInteger>();
+
+        public SupplySheet(BigInteger parentBalance, BigInteger localBalance, BigInteger maxBalance)
+        {
+            this.ParentBalance = parentBalance;
+            this.LocalBalance = localBalance;
+            this._maxBalance = maxBalance;
+        }
+
+        public BigInteger GetChildBalance(Chain chain)
+        {
+            Throw.IfNull(chain, nameof(chain));
+
+            if (_childBalances.ContainsKey(chain.Address))
+            {
+                return _childBalances[chain.Address];
+            }
+
+            return 0;
+        }
+
+        public bool MoveToParent(BigInteger amount)
+        {
+            if (LocalBalance < amount)
+            {
+                return false;
+            }
+
+            LocalBalance -= amount;
+            ParentBalance += amount;
+            return true;
+        }
+
+        public bool MoveFromParent(BigInteger amount)
+        {
+            if (ParentBalance < amount)
+            {
+                return false;
+            }
+
+            LocalBalance += amount;
+            ParentBalance -= amount;
+            return true;
+        }
+
+        public bool MoveToChild(Chain child, BigInteger amount)
+        {
+            Throw.IfNull(child, nameof(child));
+
+            if (LocalBalance < amount)
+            {
+                return false;
+            }
+
+            LocalBalance -= amount;
+
+            var childBalance = GetChildBalance(child);
+            childBalance += amount;
+            _childBalances[child.Address] = childBalance;
+
+            return true;
+        }
+
+        public bool MoveFromChild(Chain child, BigInteger amount)
+        {
+            Throw.IfNull(child, nameof(child));
+
+            var childBalance = GetChildBalance(child);
+
+            if (childBalance < amount)
+            {
+                return false;
+            }
+
+            LocalBalance += amount;
+
+            childBalance -= amount;
+            _childBalances[child.Address] = childBalance;
+
+            return true;
+        }
+
+        public bool Burn(BigInteger amount)
+        {
+            if (LocalBalance < amount)
+            {
+                return false;
+            }
+
+            LocalBalance -= amount;
+            return true;
+        }
+
+        public bool Mint(BigInteger amount)
+        {
+            BigInteger existingSupply = ParentBalance + LocalBalance;
+
+            foreach (var childBalance in _childBalances.Values)
+            {
+                existingSupply += childBalance;
+            }
+
+            var expectedSupply = existingSupply + amount;
+
+            if (expectedSupply > _maxBalance)
+            {
+                return false;
+            }
+
+            LocalBalance += amount;
+            return true;
+        }
+
+    }
+}
