bdcc69a7409e6b4e8a05be749f207fd9d8279bfe
Sergio Flores <relfos@gmail.com>
2018-12-04 19:38:28 +0000

    Added Epoch methods to stake contract

--- Phantasma.Blockchain/Contracts/Native/StakeContract.cs
+++ Phantasma.Blockchain/Contracts/Native/StakeContract.cs
@@ -10,11 +10,20 @@ namespace Phantasma.Blockchain.Contracts.Native
         public Address address;
         public BigInteger stake;
         public Timestamp timestamp;
+        public int slashes;
     }
 
     public sealed class StakeContract : SmartContract
     {
         public override string Name => "stake";
+
+        public static readonly uint EpochDurationInSeconds = 60;
+        public static readonly uint EpochSlashLimitInSeconds = 5;
+
+        private Timestamp _epochStart;
+        private Address _currentLeader;
+        private Collection<Address> _epochParticipants;
+        private BigInteger _unclaimedFees;
 
         private Collection<Address> _entryList;
         private Map<Address, ValidatorInfo> _entryMap;
@@ -67,15 +76,23 @@ namespace Phantasma.Blockchain.Contracts.Native
                 address = address,
                 stake = stakeAmount,
                 timestamp = Timestamp.Now,
+                slashes = 0
             };
             _entryMap.Set(address, entry);
+
+            // if there is no leader yet, create a new epoch with this as leader
+            if (_entryList.Count() == 1)
+            {
+                _currentLeader = address;
+                _epochStart = Timestamp.Now;
+                // TODO more required?
+            }
         }
 
         public void Unstake(Address address)
         {
+            Runtime.Expect(IsValidator(address), "validator failed");
             Runtime.Expect(IsWitness(address), "witness failed");
-
-            Runtime.Expect(_entryMap.ContainsKey(address), "not a validator address");
 
             var entry = _entryMap.Get(address);
 
@@ -99,7 +116,102 @@ namespace Phantasma.Blockchain.Contracts.Native
 
         public BigInteger GetStake(Address address)
         {
-            throw new System.NotImplementedException();
+            Runtime.Expect(_entryMap.ContainsKey(address), "not a validator address");
+            var entry = _entryMap.Get(address);
+            return entry.stake;
+        }
+
+        private void AccumulateFees()
+        {
+            var txFee = Runtime.Chain.GetBlockReward(Runtime.Block);
+            _unclaimedFees += txFee;
+
+            var token = this.Runtime.Nexus.NativeToken;
+            var balances = this.Runtime.Chain.GetTokenBalances(token);
+            //Runtime.Expect(token.Burn(balances, from, amount), "burn failed");
+        }
+
+        public void ContinueEpoch(Address address)
+        {
+            Runtime.Expect(IsValidator(address), "validator failed");
+            Runtime.Expect(IsWitness(address), "witness failed");
+
+            Runtime.Expect(address == _currentLeader, "not epoch address");
+
+            var currentTime = Timestamp.Now;
+            var diff = currentTime - _epochStart;
+            Runtime.Expect(diff < EpochDurationInSeconds, "too late");
+
+            if (!_epochParticipants.Contains(address))
+            {
+                _epochParticipants.Add(address);
+            }
+
+            AccumulateFees();
+        }
+
+        private void DistributeEpoch(bool slashed)
+        {
+            var count = _epochParticipants.Count();
+            var distributionAmount = _unclaimedFees / count;
+            var leftovers = _unclaimedFees - (distributionAmount * count);
+
+            var token = Runtime.Nexus.NativeToken;
+            var balances = Runtime.Chain.GetTokenBalances(token);
+            
+            for (int i = 0; i < count; i++)
+            {
+                var participant = _epochParticipants.Get(i);
+                BigInteger amountToReceive = 0;
+
+                if (participant == _currentLeader)
+                {
+                    amountToReceive += leftovers;
+
+                    if (!slashed)
+                    {
+                        amountToReceive = distributionAmount;
+                    }
+                }
+                else
+                {
+                    amountToReceive = distributionAmount;
+                }
+
+                Runtime.Expect(token.Mint(balances, participant, amountToReceive), "mint failed");
+            }
+
+            _unclaimedFees = 0;
+            // _currentLeader = _nextLeader; TODO
+        }
+
+        public void CloseEpoch(Address address)
+        {
+            Runtime.Expect(IsValidator(address), "validator failed");
+            Runtime.Expect(IsWitness(address), "witness failed");
+
+            Runtime.Expect(_unclaimedFees > 0, "epoch not active");
+            Runtime.Expect(address == _currentLeader, "not epoch address");
+
+            var currentTime = Timestamp.Now;
+            var diff = currentTime - _epochStart;
+            Runtime.Expect(diff >= EpochDurationInSeconds, "too soon");
+
+            diff -= EpochDurationInSeconds;
+
+            if (address == _currentLeader)
+            {
+                Runtime.Expect(diff < EpochSlashLimitInSeconds, "too late, slashed");
+            }
+            else
+            {
+                Runtime.Expect(diff >= EpochSlashLimitInSeconds, "too soon for slash");
+            }
+
+            AccumulateFees();
+
+            var slashed = address != _currentLeader;
+            DistributeEpoch(slashed);
         }
 
     }
