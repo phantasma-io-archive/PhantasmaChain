c91fa4c9140b932b081c6f85f5f852aaf0a521e2
Sergio Flores <relfos@gmail.com>
2018-12-21 18:25:59 +0000

    Added Success() to Logger

--- Phantasma.Core/Log/ConsoleLogger.cs
+++ Phantasma.Core/Log/ConsoleLogger.cs
@@ -16,6 +16,7 @@ namespace Phantasma.Core.Log
                     case LogEntryKind.Error: Console.ForegroundColor = ConsoleColor.Red; break;
                     case LogEntryKind.Warning: Console.ForegroundColor = ConsoleColor.Yellow; break;
                     case LogEntryKind.Message: Console.ForegroundColor = ConsoleColor.Gray; break;
+                    case LogEntryKind.Sucess: Console.ForegroundColor = ConsoleColor.Green; break;
                     case LogEntryKind.Debug: Console.ForegroundColor = ConsoleColor.Cyan; break;
                     default: return;
                 }
--- Phantasma.Core/Log/LogKind.cs
+++ Phantasma.Core/Log/LogKind.cs
@@ -7,6 +7,7 @@
     {
         None,
         Message,
+        Sucess,
         Warning,
         Error,
         Debug,
