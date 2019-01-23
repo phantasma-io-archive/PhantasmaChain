b392e69757b05b8185a0791b012e4d71b505133d
Bernardo Pinho <bernardopinho@gmail.com>
2019-01-23 12:14:37 +0000

    Added more VM opcode tests

--- Phantasma.Tests/VMTests.cs
+++ Phantasma.Tests/VMTests.cs
@@ -5,6 +5,7 @@ using Phantasma.Numerics;
 using Phantasma.Cryptography;
 using Phantasma.VM;
 using System.Collections.Generic;
+using System.Text;
 using Phantasma.Blockchain;
 using Phantasma.VM.Utils;
 
@@ -15,8 +16,32 @@ namespace Phantasma.Tests
         private Dictionary<string, Func<ExecutionFrame, ExecutionState>> _interops = new Dictionary<string, Func<ExecutionFrame, ExecutionState>>();
         private Func<string, ExecutionContext> _contextLoader;
 
+        public enum DebugEnum
+        {
+            enum1,
+            enum2,
+            enum3
+        }
+
+        public struct DebugStruct
+        {
+            public int x;
+            public int y;
+        }
+
+        public class DebugClass
+        {
+            public int x;
+
+            public DebugClass(int y)
+            {
+                x = y;
+            }
+        }
+
         public TestVM(byte[] script) : base(script)
         {
+            RegisterDefaultInterops();
         }
 
 #if DEBUG
@@ -64,6 +89,75 @@ namespace Phantasma.Tests
                 var str = obj.AsString();
                 str = str.ToUpper();
                 frame.VM.Stack.Push(VMObject.FromObject(str));
+                return ExecutionState.Running;
+            });
+
+            RegisterInterop("PushEnum", (frame) =>
+            {
+                var obj = frame.VM.Stack.Pop();
+                var n = obj.AsNumber();
+                DebugEnum enm;
+
+                switch (n.ToDecimal())
+                {
+                    case "1":
+                        enm = DebugEnum.enum1;
+                        break;
+                    case "2":
+                        enm = DebugEnum.enum2;
+                        break;
+                    default:
+                        enm = DebugEnum.enum3;
+                        break;
+                }
+                
+                frame.VM.Stack.Push(VMObject.FromObject(enm));
+                return ExecutionState.Running;
+            });
+
+            RegisterInterop("PushDebugClass", (frame) =>
+            {
+                var obj = frame.VM.Stack.Pop();
+                int n = (int)obj.AsNumber();
+
+                DebugClass dbClass = new DebugClass(n);
+                
+                frame.VM.Stack.Push(VMObject.FromObject(dbClass));
+                return ExecutionState.Running;
+            });
+
+            RegisterInterop("IncrementDebugClass", (frame) =>
+            {
+                var obj = frame.VM.Stack.Pop();
+                var dbClass = obj.AsInterop<DebugClass>();
+
+                dbClass.x++;
+
+                frame.VM.Stack.Push(VMObject.FromObject(dbClass));
+                return ExecutionState.Running;
+            });
+
+            RegisterInterop("PushBytes", (frame) =>
+            {
+                var obj = frame.VM.Stack.Pop();
+                string str = obj.AsString();
+
+                var byteArray = Encoding.ASCII.GetBytes(str);
+
+                frame.VM.Stack.Push(VMObject.FromObject(byteArray));
+                return ExecutionState.Running;
+            });
+
+            RegisterInterop("PushDebugStruct", (frame) =>
+            {
+                var obj = frame.VM.Stack.Pop();
+                int n = (int) obj.AsNumber();
+
+                DebugStruct dbStruct = new DebugStruct();
+                dbStruct.x = n;
+                dbStruct.y = n;
+
+                frame.VM.Stack.Push(VMObject.FromObject(dbStruct));
                 return ExecutionState.Running;
             });
         } 
