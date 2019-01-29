19f0270bc6956c0cccef5a8a075356c8065bb5a8
Sergio Flores <relfos@gmail.com>
2019-01-29 19:18:38 +0000

    Paylib support for Phantasma balance fetching

--- Phantasma.Pay/Chains/PhantasmaWallet.cs
+++ Phantasma.Pay/Chains/PhantasmaWallet.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Phantasma.Blockchain.Tokens;
 using Phantasma.Cryptography;
+using Phantasma.Numerics;
 
 namespace Phantasma.Pay.Chains
 {
@@ -20,6 +22,32 @@ namespace Phantasma.Pay.Chains
 
         public override void SyncBalances(Action<bool> callback)
         {
+            _balances.Clear();
+
+            var url = "http://localhost:7078/api/getAccount/" + this.Address; // TODO change this later
+            JSONRequest(url, (root) =>
+            {
+                if (root == null)
+                {
+                    callback(false);
+                    return;
+                }
+
+                root = root.GetNode("balances");
+                foreach (var child in root.Children)
+                {
+                    var symbol = child.GetString("symbol");
+                    var decimals = child.GetInt32("decimals");
+
+                    var temp = child.GetString("amount");
+                    var n = BigInteger.Parse(temp);
+                    var amount = TokenUtils.ToDecimal(n, decimals);
+
+                    _balances.Add(new WalletBalance(symbol, amount));
+                }
+
+                callback(true);
+            });
             callback(false);
         }
 
