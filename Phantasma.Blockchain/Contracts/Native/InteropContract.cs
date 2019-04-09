88174fa41c0463ce5a3baea4364b985a8e312e3e
Sérgio Flores <relfos@gmail.com>
2019-04-09 17:35:45 +0100

    Some fixes for interop contact

--- Phantasma.Blockchain/Contracts/Native/InteropContract.cs
+++ Phantasma.Blockchain/Contracts/Native/InteropContract.cs
@@ -1,4 +1,5 @@
-﻿using Phantasma.Blockchain.Tokens;
+﻿using Phantasma.Blockchain.Storage;
+using Phantasma.Blockchain.Tokens;
 using Phantasma.Cryptography;
 using Phantasma.Numerics;
 
@@ -7,6 +8,8 @@ namespace Phantasma.Blockchain.Contracts.Native
     public sealed class InteropContract : SmartContract
     {
         public override string Name => "interop";
+
+        private StorageMap _hashes; 
 
         public InteropContract() : base()
         {
@@ -25,11 +28,31 @@ namespace Phantasma.Blockchain.Contracts.Native
             Runtime.Expect(token.Flags.HasFlag(TokenFlags.Transferable), "token must be transferable");
             Runtime.Expect(token.Flags.HasFlag(TokenFlags.External), "token must be external");
 
+            var chainHashes = _hashes.Get<string, StorageSet>(symbol);
+            Runtime.Expect(!chainHashes.Contains<Hash>(hash), "hash already seen");
+            chainHashes.Add<Hash>(hash);
+
+            var minimumAmount = UnitConversion.ToBigInteger(1, token.Decimals);
+            // TODO this might have to smaller eg: for BTC
+            Runtime.Expect(amount >= minimumAmount, "minimum amount not reached");
+
             var source = Address.Null;
 
             var balances = this.Runtime.Chain.GetTokenBalances(token);
             var supplies = token.IsCapped ? Runtime.Chain.GetTokenSupplies(token) : null;
             Runtime.Expect(token.Mint(this.Storage, balances, supplies, destination, amount), "mint failed");
+
+            if (symbol == Nexus.StableTokenSymbol)
+            {
+                var feeAmount = minimumAmount / 10;
+                Runtime.Expect(token.Transfer(this.Storage, balances, destination, Runtime.Chain.Address, feeAmount), "fee transfer failed");
+
+                var fuelToken = Runtime.Nexus.FuelToken;
+                var fuelBalances = this.Runtime.Chain.GetTokenBalances(fuelToken);
+                var fuelSupplies = token.IsCapped ? Runtime.Chain.GetTokenSupplies(fuelToken) : null;
+                Runtime.Expect(fuelToken.Mint(this.Storage, fuelBalances, fuelSupplies, destination, feeAmount), "fee mint failed");
+                // TODO this mint can only be done if the maximum supply was not reached yet
+            }
 
             Runtime.Notify(EventKind.TokenReceive, destination, new TokenEventData() { chainAddress = this.Runtime.Chain.Address, value = amount, symbol = symbol });
         }
