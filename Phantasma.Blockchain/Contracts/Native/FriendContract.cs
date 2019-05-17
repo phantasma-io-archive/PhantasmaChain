739be12ae1a1b439733ec65b67ee5ec5c681efc7
Joao Catarino <joaopereiracatarino@gmail.com>
2019-05-17 20:07:03 +0100

    Updated FriendContract events

--- Phantasma.Blockchain/Contracts/Native/FriendContract.cs
+++ Phantasma.Blockchain/Contracts/Native/FriendContract.cs
@@ -22,7 +22,7 @@ namespace Phantasma.Blockchain.Contracts.Native
 
             friendList.Add(friend);
 
-            Runtime.Notify(EventKind.AddressAdd, target, friend);
+            Runtime.Notify(EventKind.AddFriend, target, friend);
         }
 
         public void RemoveFriend(Address target, Address friend)
@@ -37,7 +37,7 @@ namespace Phantasma.Blockchain.Contracts.Native
             Runtime.Expect(friendList.Contains(friend), "friend not found");
             friendList.Remove(friend);
 
-            Runtime.Notify(EventKind.AddressRemove, target, friend);
+            Runtime.Notify(EventKind.RemoveFriend, target, friend);
         }
 
         public Address[] GetFriends(Address target)
