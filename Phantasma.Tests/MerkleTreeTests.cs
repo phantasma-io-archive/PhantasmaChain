dbb22af0d315f98490d2c87db0fb402cebdb6030
Bernardo <bernardopinho@gmail.com>
2019-07-12 19:08:27 +0100

    Added MerkleTree tests

---
+++ Phantasma.Tests/MerkleTreeTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phantasma.Cryptography;
+
+namespace Phantasma.Tests
+{
+    [TestClass]
+    public class MerkleTreeTests
+    {
+        [TestMethod]
+        public void TestMerkleSuccess()
+        {
+            uint fileSize = 1000;
+            uint chunkSize = fileSize / 10;
+
+            byte[] file = new byte[fileSize];
+
+            Random r = new Random();
+            r.NextBytes(file);
+
+            var tree = new MerkleTree(file, chunkSize);
+
+            var chunkCount = file.Length / chunkSize;
+            if (chunkCount * chunkSize < file.Length)
+            {
+                chunkCount++;
+            }
+
+            var chunk = new byte[chunkSize];
+            for (int i = 0; i < chunkCount; i++)
+            {
+                Array.Copy(file, i * chunkSize, chunk, 0, chunkSize);
+                Assert.IsTrue(tree.VerifyContent(chunk, (uint)i));
+            }
+        }
+
+        [TestMethod]
+        public void TestMerkleFailure()
+        {
+            uint fileSize = 1000;
+            uint chunkSize = fileSize / 10;
+
+            byte[] file = new byte[fileSize];
+
+            Random r = new Random();
+            r.NextBytes(file);
+
+            var tree = new MerkleTree(file, chunkSize);
+
+            var chunkCount = file.Length / chunkSize;
+            if (chunkCount * chunkSize < file.Length)
+            {
+                chunkCount++;
+            }
+
+            var originalChunk = new byte[chunkSize];
+
+            var fakeChunk = new byte[chunkSize];
+            r.NextBytes(fakeChunk);
+
+            for (int i = 0; i < chunkCount; i++)
+            {
+                Array.Copy(file, i * chunkSize, originalChunk, 0, chunkSize);
+
+                while (fakeChunk.SequenceEqual(originalChunk))
+                {
+                    r.NextBytes(fakeChunk);
+                }
+
+                Assert.IsFalse(tree.VerifyContent(fakeChunk, (uint)i));
+            }
+        }
+    }
+}
