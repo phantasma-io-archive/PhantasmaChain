afc8b5a199606d01f0bc7e4d6ed7050573b41f90
Bernardo Pinho <bernardopinho@gmail.com>
2019-08-21 17:42:29 +0100

    Updated Merkle Tree tests for fixed chunk size

--- Phantasma.Tests/MerkleTreeTests.cs
+++ Phantasma.Tests/MerkleTreeTests.cs
@@ -12,10 +12,10 @@ namespace Phantasma.Tests
     public class MerkleTreeTests
     {
         [TestMethod]
-        public void TestMerkleSuccess()
+        public void TestSingleNodeMerkleSuccess()
         {
             uint fileSize = 1000;
-            uint chunkSize = fileSize / 10;
+            uint chunkSize = MerkleTree.ChunkSize;
 
             byte[] file = new byte[fileSize];
 
@@ -30,19 +30,21 @@ namespace Phantasma.Tests
                 chunkCount++;
             }
 
-            var chunk = new byte[chunkSize];
+            var actualChunkSize = MerkleTree.ChunkSize < fileSize ? MerkleTree.ChunkSize : fileSize;
+
+            var chunk = new byte[actualChunkSize];
             for (int i = 0; i < chunkCount; i++)
             {
-                Array.Copy(file, i * chunkSize, chunk, 0, chunkSize);
+                Array.Copy(file, i * actualChunkSize, chunk, 0, actualChunkSize);
                 Assert.IsTrue(tree.VerifyContent(chunk, i));
             }
         }
 
         [TestMethod]
-        public void TestMerkleFailure()
+        public void TestSingleNodeMerkleFailure()
         {
             uint fileSize = 1000;
-            uint chunkSize = fileSize / 10;
+            uint chunkSize = MerkleTree.ChunkSize;
 
             byte[] file = new byte[fileSize];
 
@@ -57,19 +59,87 @@ namespace Phantasma.Tests
                 chunkCount++;
             }
 
-            var originalChunk = new byte[chunkSize];
+            var actualChunkSize = MerkleTree.ChunkSize < fileSize ? MerkleTree.ChunkSize : fileSize;
+            var originalChunk = new byte[actualChunkSize];
 
-            var fakeChunk = new byte[chunkSize];
+            var fakeChunk = new byte[actualChunkSize];
             r.NextBytes(fakeChunk);
 
             for (int i = 0; i < chunkCount; i++)
             {
-                Array.Copy(file, i * chunkSize, originalChunk, 0, chunkSize);
+                Array.Copy(file, i * actualChunkSize, originalChunk, 0, actualChunkSize);
 
                 while (fakeChunk.SequenceEqual(originalChunk))
                 {
                     r.NextBytes(fakeChunk);
                 }
+
+                Assert.IsFalse(tree.VerifyContent(fakeChunk, i));
+            }
+        }
+
+        [TestMethod]
+        public void TestMultipleNodeMerkleSuccess()
+        {
+            uint chunkSize = MerkleTree.ChunkSize;
+            uint fileSize = (uint) (chunkSize * 20.9);
+
+            byte[] file = new byte[fileSize];
+
+            Random r = new Random();
+            r.NextBytes(file);
+
+            var tree = new MerkleTree(file);
+
+            var chunkCount = file.Length / chunkSize;
+            if (chunkCount * chunkSize < file.Length)
+            {
+                chunkCount++;
+            }
+
+            for (int i = 0; i < chunkCount; i++)
+            {
+                var leftoverFile = fileSize - (MerkleTree.ChunkSize * i);
+                var actualChunkSize = MerkleTree.ChunkSize < leftoverFile ? MerkleTree.ChunkSize : leftoverFile;
+                var chunk = new byte[actualChunkSize];
+
+                Array.Copy(file, i * MerkleTree.ChunkSize, chunk, 0, actualChunkSize);
+                Assert.IsTrue(tree.VerifyContent(chunk, i));
+            }
+        }
+
+        [TestMethod]
+        public void TestMultipleNodeMerkleFailure()
+        {
+            uint chunkSize = MerkleTree.ChunkSize;
+            uint fileSize = (uint)(chunkSize * 20.9);
+
+            byte[] file = new byte[fileSize];
+
+            Random r = new Random();
+            r.NextBytes(file);
+
+            var tree = new MerkleTree(file);
+
+            var chunkCount = file.Length / chunkSize;
+            if (chunkCount * chunkSize < file.Length)
+            {
+                chunkCount++;
+            }
+            
+            for (int i = 0; i < chunkCount; i++)
+            {
+                var leftoverFile = fileSize - (MerkleTree.ChunkSize * i);
+                var actualChunkSize = MerkleTree.ChunkSize < leftoverFile ? MerkleTree.ChunkSize : leftoverFile;
+
+                var originalChunk = new byte[actualChunkSize];
+                Array.Copy(file, i * MerkleTree.ChunkSize, originalChunk, 0, actualChunkSize);
+
+                var fakeChunk = new byte[actualChunkSize];
+                do
+                {
+                    r.NextBytes(fakeChunk);
+                } while (fakeChunk.SequenceEqual(originalChunk));
 
                 Assert.IsFalse(tree.VerifyContent(fakeChunk, i));
             }
