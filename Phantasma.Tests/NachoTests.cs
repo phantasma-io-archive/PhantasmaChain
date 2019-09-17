173d30340245de52ad9e484c204c5132e432c340
Joao Catarino <joaopereiracatarino@gmail.com>
2019-09-17 17:07:25 +0100

    Added new NachoTest

---
+++ Phantasma.Tests/NachoTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phantasma.API;
+using Phantasma.Blockchain.Contracts.Native;
+using Phantasma.Cryptography;
+using Phantasma.Numerics;
+using Phantasma.Simulator;
+using Phantasma.Storage;
+using Phantasma.VM;
+using Phantasma.VM.Utils;
+
+namespace Phantasma.Tests
+{
+    [TestClass]
+    class NachoTests
+    {
+        [TestMethod]
+        public void TestGetBotWrestler()
+        {
+            var owner       = KeyPair.Generate();
+            var simulator   = new ChainSimulator(owner, 1234);
+            var nexus       = simulator.Nexus;
+            var api         = new NexusAPI(nexus);
+
+
+            var callScript      = ScriptUtils.BeginScript().CallContract("nacho", "GetWrestler", new object[] {-1}).EndScript();
+            var apiResult       = (ScriptResult)api.InvokeRawScript("main", Base16.Encode(callScript));
+            var bytes           = Base16.Decode(apiResult.result);
+            var objResult       = Serialization.Unserialize<VMObject>(bytes);
+            var nachoWrestler   = objResult.ToStruct<NachoWrestler>();
+
+        }
+
+    }
+}
