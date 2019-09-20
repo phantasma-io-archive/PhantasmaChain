a04b33d8a27d48e349813b9d06eedb8c8dbdecaa
Sérgio Flores <relfos@gmail.com>
2019-09-20 19:22:35 +0100

    Added consensus voting code

--- Phantasma.Blockchain/Contracts/Native/ConsensusContract.cs
+++ Phantasma.Blockchain/Contracts/Native/ConsensusContract.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Phantasma.Core.Types;
 using Phantasma.Cryptography;
 using Phantasma.Numerics;
@@ -33,17 +34,24 @@ namespace Phantasma.Blockchain.Contracts.Native
         public BigInteger votes;
     }
 
+    public struct PollVote
+    {
+        public BigInteger index;
+        public BigInteger percentage;
+    }
+
     public struct ConsensusPoll
     {
         public string subject;
         public ConsensusKind kind;
         public ConsensusMode mode;
         public PollState state;
-        public PollValue[] values;
+        public PollValue[] entries;
         public BigInteger selected;
         public BigInteger round;
         public Timestamp startTime;
         public Timestamp endTime;
+        public BigInteger votesPerUser;
         public byte[] script;
     }
 
@@ -58,8 +66,12 @@ namespace Phantasma.Blockchain.Contracts.Native
         public override string Name => "consensus";
 
         private StorageMap _pollMap; //<Address> 
+        private StorageList _pollList; 
         private StorageMap _presences; // address, List<PollPresence>
 
+        public const int MaxEntriesPerPoll = 100;
+        private const int MinimumPollLength = 86400;
+        private const int MaximumPollLength = MinimumPollLength * 90;
         public const int PollVoteLimit = 50000;
 
         public const string SystemPoll = "system.";
@@ -82,6 +94,7 @@ namespace Phantasma.Blockchain.Contracts.Native
                 if (Runtime.Time >= poll.startTime && Runtime.Time<poll.endTime && poll.state == PollState.Inactive)
                 {
                     poll.state = PollState.Active;
+                    _pollList.Add<string>(subject);
                 }
                 else
                 if (Runtime.Time >= poll.endTime && poll.state == PollState.Active)
@@ -92,9 +105,9 @@ namespace Phantasma.Blockchain.Contracts.Native
                     BigInteger totalVotes = 0;
                     int ties = 0;
 
-                    for (int i=0; i<poll.values.Length; i++)
+                    for (int i=0; i<poll.entries.Length; i++)
                     {
-                        var entry = poll.values[i];
+                        var entry = poll.entries[i];
                         totalVotes += entry.votes;
 
                         if (entry.votes > bestVotes)
@@ -145,25 +158,138 @@ namespace Phantasma.Blockchain.Contracts.Native
             return poll;
         }
 
-        public void InitPoll(Address from, string subject, ConsensusKind kind, ConsensusMode mode, byte[] script)
+        public void InitPoll(Address from, string subject, ConsensusKind kind, ConsensusMode mode, Timestamp startTime, Timestamp endTime, PollValue[] entries, BigInteger votesPerUser, byte[] script)
         {
             if (subject.StartsWith(SystemPoll))
             {
                 Runtime.Expect(IsValidator(from), "must be validator");
             }
+
+            Runtime.Expect(Runtime.Chain.IsRoot, "not root chain");
+
+            Runtime.Expect(kind == ConsensusKind.Validators, "community polls not yet");
+
+            Runtime.Expect(entries.Length > 1, "invalid amount of entries");
+            Runtime.Expect(entries.Length <= MaxEntriesPerPoll, "too many entries");
+
+            Runtime.Expect(startTime >= Runtime.Time, "invalid start time");
+            var minEndTime = new Timestamp(startTime.Value + MinimumPollLength);
+            var maxEndTime = new Timestamp(startTime.Value + MaximumPollLength);
+            Runtime.Expect(endTime >= minEndTime, "invalid end time");
+            Runtime.Expect(endTime <= maxEndTime, "invalid end time");
+
+            Runtime.Expect(script != null && script.Length > 0, "invalid script");
+
+            Runtime.Expect(votesPerUser > 0, "number of votes per user too low");
+            Runtime.Expect(votesPerUser < entries.Length, "number of votes per user too high");
+
+            Runtime.Expect(IsWitness(from), "invalid witness");
+
+            ConsensusPoll poll;
+            if (_pollMap.ContainsKey<string>(subject))
+            {
+                poll = FetchPoll(subject);
+                Runtime.Expect(poll.state == PollState.Consensus || poll.state == PollState.Failure, "poll already in progress");
+                poll.round += 1;
+                poll.state = PollState.Inactive;
+            }
+            else
+            {
+                poll = new ConsensusPoll();
+                poll.subject = subject;
+                poll.round = 1;
+            }
+
+            poll.startTime = startTime;
+            poll.endTime = endTime;
+            poll.entries = entries;
+            poll.kind = kind;
+            poll.mode = mode;
+            poll.script = script;
+            poll.selected = -1;
+            poll.state = PollState.Inactive;
+            poll.votesPerUser = votesPerUser;
+
+            _pollMap.Set<string, ConsensusPoll>(subject, poll);
         }
 
-        public void Vote(Address from, string subject)
+        public void SingleVote(Address from, string subject, BigInteger index)
+        {
+            MultiVote(from, subject, new PollVote[] { new PollVote() { index = index, percentage = 100 } });
+        }
+
+        public void MultiVote(Address from, string subject, PollVote[] choices)
         {
             Runtime.Expect(_pollMap.ContainsKey<string>(subject), "invalid subject");
+
+            Runtime.Expect(choices.Length > 0, "invalid number of choices");
 
             var poll = FetchPoll(subject);
 
             Runtime.Expect(poll.state == PollState.Active, "poll not active");
 
+            Runtime.Expect(choices.Length <= poll.votesPerUser, "too many choices");
+
             Runtime.Expect(IsWitness(from), "invalid witness");
 
             var presences = _presences.Get<Address, StorageList>(from);
+            var count = presences.Count();
+            int index = -1;
+            BigInteger round = 0;
+
+            for (int i=0; i<count; i++)
+            {
+                var presence = presences.Get<PollPresence>(i);
+                if (presence.subject == subject)
+                {
+                    index = -1;
+                    round = presence.round;
+                    break;
+                }
+            }
+
+            if (index >= 0)
+            {
+                Runtime.Expect(round < poll.round, "already voted");
+            }
+
+            BigInteger votingPower;
+
+            if (poll.kind == ConsensusKind.Validators)
+            {
+                votingPower = 100;
+            }
+            else
+            {
+                votingPower = (BigInteger)Runtime.CallContext("energy", "GetAddressVotingPower", from); 
+            }
+
+            Runtime.Expect(votingPower > 0, "not enough voting power");
+
+            for (int i=0; i<choices.Length; i++)
+            {
+                var votes = (votingPower * choices[i].percentage) / 100;
+                Runtime.Expect(votes > 0, "choice percentage is too low");
+
+                var targetIndex = (int)choices[i].index;
+                poll.entries[targetIndex].votes += votes;
+            }
+
+            // finally add this voting round to the presences list
+            var temp = new PollPresence()
+            {
+                subject = subject,
+                round = poll.round,
+            };
+
+            if (index >= 0)
+            {
+                presences.Replace<PollPresence>(index, temp);
+            }
+            else
+            {
+                presences.Add(temp);
+            }
         }
 
         public bool HasConsensus(string subject, byte[] value)
@@ -172,9 +298,9 @@ namespace Phantasma.Blockchain.Contracts.Native
 
             var poll = FetchPoll(subject);
 
-            if (poll.state == PollState.Consensus && poll.selected>=0 && poll.selected<poll.values.Length)
+            if (poll.state == PollState.Consensus && poll.selected>=0 && poll.selected<poll.entries.Length)
             {
-                var winner = poll.values[(int)poll.selected].value;
+                var winner = poll.entries[(int)poll.selected].value;
                 return value.SequenceEqual(winner);
             }
 
