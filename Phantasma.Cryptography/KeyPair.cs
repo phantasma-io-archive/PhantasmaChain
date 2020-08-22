f6b3d3c0657d1574c8360124abaeedf293beb9b8
alexmpa <62516783+alexmpa@users.noreply.github.com>
2020-08-22 14:13:02 +0300

    Add comment

--- Phantasma.Cryptography/KeyPair.cs
+++ Phantasma.Cryptography/KeyPair.cs
@@ -11,6 +11,8 @@ namespace Phantasma.Cryptography
         byte[] PrivateKey { get; }
         byte[] PublicKey { get; }
 
+        // byte[] customSignFunction(byte[] message, byte[] prikey, byte[] pubkey)
+        // allows singning with custom crypto libs.
         Signature Sign(byte[] msg, Func<byte[], byte[], byte[], byte[]> customSignFunction = null);
     }
 
