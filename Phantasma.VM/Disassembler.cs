2c0e50dc68c7c7018c69f0be3b13e9d103950e46
Sergio Flores <relfos@gmail.com>
2020-11-22 19:29:02 +0000

    Fixed disasemble (throw opcode bug)

--- Phantasma.VM/Disassembler.cs
+++ Phantasma.VM/Disassembler.cs
@@ -69,6 +69,7 @@ namespace Phantasma.VM
                     case Opcode.POP:
                     case Opcode.PUSH:
                     case Opcode.EXTCALL:
+                    case Opcode.THROW:
                         {
                             var src = Read8();
                             temp.Args = new object[] { src };
@@ -101,25 +102,6 @@ namespace Phantasma.VM
                                 var newPos = (short)Read16();
                                 temp.Args = new object[] { src, newPos };
                             }
-                            break;
-                        }
-
-                    // args: var length, var bytes
-                    case Opcode.THROW:
-                        {
-                            var len = (int)ReadVar(1024);
-
-                            byte[] bytes;
-                            if (len > 0)
-                            {
-                                bytes = ReadBytes(len);
-                            }
-                            else
-                            {
-                                bytes = new byte[0];
-                            }
-
-                            temp.Args = new object[] { len, bytes };
                             break;
                         }
 
