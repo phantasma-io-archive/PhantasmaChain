520a5e53fa8dfff3e78c360c8b8d38d6df6e29ee
Sergio Flores <relfos@gmail.com>
2020-12-16 12:49:16 +0000

    More work in debugger

--- Phantasma.VM/Debug/DebugHost.cs
+++ Phantasma.VM/Debug/DebugHost.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace Phantasma.VM.Debug
 {
@@ -11,9 +15,105 @@ namespace Phantasma.VM.Debug
 
         private Queue<DebugCommand> _commands = new Queue<DebugCommand>();
 
+        private TcpListener _server;
+        private NetworkStream _stream;
+        private StringBuilder _buffer;
+
+        private bool _active;
+
         public DebugHost()
         {
+            _active = true;
+            var socketThread = new Thread(SocketThread);
+            socketThread.Start();
+        }
 
+        private void SocketThread()
+        {
+            var localAddr = IPAddress.Parse("127.0.0.1");
+
+            try
+            {
+                _server = new TcpListener(localAddr, Debugger.Port);
+                _server.Start();
+
+                // Enter the listening loop.
+                while (_active)
+                {
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = _server.AcceptTcpClient();
+
+                    // Get a stream object for reading and writing
+                    _stream = client.GetStream();
+
+                    var data = Receive();
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    // Send back a response.
+                    Send(data);
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                _server.Stop();
+            }
+        }
+
+        private string Receive()
+        {
+            var bytes = new Byte[256];
+
+            string result = null;
+
+            // Loop to receive all the data sent by the client
+            do
+            {
+                var read = _stream.Read(bytes, 0, bytes.Length);
+                if (read == 0)
+                {
+                    return null;
+                }
+
+                for (int i = 0; i < read; i++)
+                {
+                    var ch = (char)_buffer[i];
+
+                    if (ch == '\n')
+                    {
+                        if (result == null)
+                        {
+                            result = _buffer.ToString();
+                            _buffer.Clear();
+                        }
+                    }
+                    else
+                    {
+                        _buffer.Append(ch);
+                    }
+                }
+            } while (result == null);
+
+            return result;
+        }
+
+        private void Send(string message)
+        {
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+
+            // Send back a response.
+            _stream.Write(msg, 0, msg.Length);
+            Console.WriteLine("Sent: {0}", message);
         }
 
         public void OnStep(ExecutionFrame frame, Stack<VMObject> stack)
--- Phantasma.VM/Debug/Debugger.cs
+++ Phantasma.VM/Debug/Debugger.cs
@@ -1,9 +1,91 @@
 ﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Phantasma.VM.Debug
 {
-    public class Debugger
+    public enum DebugOpcode
     {
+        Invalid,
+        Log,
+        Add,
+        Delete,
+        Step,
+        Resume,
+    }
+
+    public struct DebugCommand
+    {
+        public readonly DebugOpcode Opcode;
+        public readonly string[] Args;
+
+        public DebugCommand(DebugOpcode opcode, IEnumerable<string> args)
+        {
+            Opcode = opcode;
+            Args = args.ToArray();
+        }
+
+        public static DebugCommand FromString(string str)
+        {
+            var split = str.Split(' ');
+            DebugOpcode opcode;
+            if (Enum.TryParse<DebugOpcode>(split[0], out opcode))
+            {
+                return new DebugCommand(opcode, split.Skip(1));
+            }
+
+            return new DebugCommand(DebugOpcode.Invalid, Enumerable.Empty<string>());
+        }
+    }
+
+    public class Debugger : IDisposable
+    {
+        public const int Port = 7671;
+
+        private TcpClient _client;
+        private NetworkStream _stream;
+
+        public Debugger(string host = "localhost", int port = Port)
+        {
+            _client = new TcpClient(host, port);
+            // Get a client stream for reading and writing.
+            _stream = _client.GetStream();
+        }
+
+        private void Send(string message)
+        {
+            // Translate the passed message into ASCII and store it as a Byte array.
+            var data = Encoding.ASCII.GetBytes(message);
+
+            // Send the message to the connected TcpServer.
+            _stream.Write(data, 0, data.Length);
+        }
+
+        private string Receive()
+        {
+            var data = new byte[256];
+
+            var bytesRead = _stream.Read(data, 0, data.Length);
+            var responseData = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+            return responseData;
+        }
+
+        public void Dispose()
+        {
+            if (_stream != null)
+            {
+                // Close everything.
+                _stream.Close();
+                _client.Close();
+
+                _stream = null;
+                _client = null;
+            }
+        }
+
         public bool AddBreakpoint(string contextName, uint offset)
         {
             throw new NotImplementedException();
