383c89ab9483ab8952bf03e2a9b23774568057ac
alexmpa <62516783+alexmpa@users.noreply.github.com>
2021-01-20 22:18:24 +0300

    RIPEMD160, SHA256, Keccak: Migrate to bouncy castle

--- Phantasma.Cryptography/CryptoExtensions.cs
+++ Phantasma.Cryptography/CryptoExtensions.cs
@@ -245,20 +245,6 @@ namespace Phantasma.Cryptography
 #endif
         }
 
-        // Secure wiping is hard
-        // * the GC can move around and copy memory
-        //   Perhaps this can be avoided by using unmanaged memory or by fixing the position of the array in memory
-        // * Swap files and error dumps can contain secret information
-        //   It seems possible to lock memory in RAM, no idea about error dumps
-        // * Compiler could optimize out the wiping if it knows that data won't be read back
-        //   I hope this is enough, suppressing inlining
-        //   but perhaps `RtlSecureZeroMemory` is needed
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static void InternalWipe(byte[] data, int offset, int count)
-        {
-            Array.Clear(data, offset, count);
-        }
-
         internal static BigInteger NextBigInteger(int sizeInBits)
         {
             if (sizeInBits < 0)
--- Phantasma.Tests/HashTests.cs
+++ Phantasma.Tests/HashTests.cs
@@ -37,67 +37,6 @@ namespace Phantasma.Tests
         }
 
         [TestMethod]
-        public void TestKeccak()
-        {
-            byte[] source = Encoding.ASCII.GetBytes(
-                "asdçflkjasçfjaçrlgjaçorigjkljbçladkfjgsaºperouiwa89tuhyjkvsldkfjçaoigfjsadfjkhsdkgjhdlkgjhdkfjbnsdflçkgsriaugfukasyfgskaruyfgsaekufygvsanfbvsdj,fhgwukaygsja,fvkusayfguwayfgsnvfuksaygfkuybhsngfukayeghsmafbsjkfgwlauifgjkshfbilçehrkluayh");
-
-            var keccak128Test = new KeccakDigest(128);
-            var keccak224Test = new KeccakDigest(224);
-            var keccak256Test = new KeccakDigest(256);
-            var keccak288Test = new KeccakDigest(288);
-            var keccak384Test = new KeccakDigest(384);
-            var keccak512Test = new KeccakDigest(512);
-
-            for (int i = 0; i < 10000; i++)
-            {
-
-                //can't find any ground truth for this one, https://8gwifi.org/MessageDigest.jsp is the only one but when comparing to other site's results for other keccaks it doesnt match up with them
-
-                /*var output1 = new byte[keccak128Test.GetDigestSize()];
-                keccak128Test.BlockUpdate(source, 0, source.Length);
-                keccak128Test.DoFinal(output1, 0);
-                var target1 = Base16.Decode("a896124a35603f3766d9d41dade89f9b");*/
-
-
-                var output2 = new byte[keccak224Test.GetDigestSize()];
-                keccak224Test.BlockUpdate(source, 0, source.Length);
-                keccak224Test.DoFinal(output2, 0);
-                var target2 = Base16.Decode("3c8aa5706aabc26dee19b466e77f8947f801762ca64316fdf3a2434a"); //https://emn178.github.io/online-tools/keccak_224.html
-
-                var output3 = new byte[keccak256Test.GetDigestSize()];
-                keccak256Test.BlockUpdate(source, 0, source.Length);
-                keccak256Test.DoFinal(output3, 0);
-                var target3 = Base16.Decode("09D3FA337D33E1BEB3C3D560D93F5FB57C66BC3E044127816F42494FA4947A92"); //https://asecuritysite.com/encryption/sha3
-
-                //can't find any ground truth for this one, https://8gwifi.org/MessageDigest.jsp is the only one but when comparing to other site's results for other keccaks it doesnt match up with them
-                /*var output4 = new byte[keccak288Test.GetDigestSize()];
-                keccak288Test.BlockUpdate(source, 0, source.Length);
-                keccak288Test.DoFinal(output4, 0);
-                var target4 = System.Convert.FromBase64String("");*/
-
-                var output5 = new byte[keccak384Test.GetDigestSize()];
-                keccak384Test.BlockUpdate(source, 0, source.Length);
-                keccak384Test.DoFinal(output5, 0);
-                var target5 = Base16.Decode("B1EA01288A8ECA553687E92943FC8E8D22B3B918462B7708FCB011B8EF28F60E7072FE2623E624DEBD00F8CF46B1F967"); //https://asecuritysite.com/encryption/sha3
-
-                var output6 = new byte[keccak512Test.GetDigestSize()];
-                keccak512Test.BlockUpdate(source, 0, source.Length);
-                keccak512Test.DoFinal(output6, 0);
-                var target6 = Base16.Decode("1057C35F3364A9C7D7EFB5B2AB48D9A71373DCA1E3680CBF6734DA5E896DD7DE2901A678240A1C936598A6C58E6253A9747E2715BBD559AA9A5DA9302B815BAC"); //https://asecuritysite.com/encryption/sha3
-
-                //Assert.IsTrue(output1.SequenceEqual(target1));
-                //Assert.IsTrue(output2.SequenceEqual(target2));
-                Assert.IsTrue(output3.SequenceEqual(target3));
-                //Assert.IsTrue(output4.SequenceEqual(target4));
-                Assert.IsTrue(output5.SequenceEqual(target5));
-                Assert.IsTrue(output6.SequenceEqual(target6));
-            }
-
-
-        }
-
-        [TestMethod]
         public void TestMurmur32()
         {
             byte[] source = Encoding.ASCII.GetBytes(
