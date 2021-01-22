bb75b3e3fbe484b32a691f21d365018442a4a2de
alexmpa <62516783+alexmpa@users.noreply.github.com>
2021-01-22 15:56:35 +0300

    Remove unused method

--- Phantasma.Cryptography/CryptoExtensions.cs
+++ Phantasma.Cryptography/CryptoExtensions.cs
@@ -130,23 +130,6 @@ namespace Phantasma.Cryptography
             return new Hashing.SHA256().ComputeHash(value, offset, count);
         }
 
-        internal static BigInteger NextBigInteger(int sizeInBits)
-        {
-            if (sizeInBits < 0)
-                throw new ArgumentException("sizeInBits must be non-negative");
-            if (sizeInBits == 0)
-                return 0;
-
-            var b = Entropy.GetRandomBytes(sizeInBits / 8 + 1);
-
-            if (sizeInBits % 8 == 0)
-                b[b.Length - 1] = 0;
-            else
-                b[b.Length - 1] &= (byte)((1 << sizeInBits % 8) - 1);
-
-            return BigInteger.FromUnsignedArray(b, isPositive: true);
-        }
-
         public static BigInteger GenerateInteger(this HMACDRBG rng, BigInteger max, int securityParameter = 64)
         {
             // The simple modular method from the NIST SP800-90A recommendation
