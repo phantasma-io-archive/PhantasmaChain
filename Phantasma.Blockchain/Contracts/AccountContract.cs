6d46ee4d23b814d5fe3cf07d052c964d1aaca1bf
alexmpa <62516783+alexmpa@users.noreply.github.com>
2021-04-25 17:44:29 +0300

    Fix unclaimed KCAL migration

--- Phantasma.Blockchain/Contracts/AccountContract.cs
+++ Phantasma.Blockchain/Contracts/AccountContract.cs
@@ -196,6 +196,12 @@ namespace Phantasma.Blockchain.Contracts
             var relayBalance = Runtime.CallNativeContext(NativeContractKind.Relay, nameof(RelayContract.GetBalance), from).AsNumber();
             Runtime.Expect(relayBalance == 0, "relay channel can't be open");
 
+            var unclaimed = Runtime.CallNativeContext(NativeContractKind.Stake, nameof(StakeContract.GetUnclaimed), from).AsNumber();
+            if (unclaimed > 0)
+            {
+                Runtime.CallNativeContext(NativeContractKind.Stake, nameof(StakeContract.Claim), from, from);
+            }
+
             var symbols = Runtime.GetTokens();
             foreach (var symbol in symbols)
             {
@@ -230,12 +236,6 @@ namespace Phantasma.Blockchain.Contracts
 
             _scriptMap.Migrate<Address, byte[]>(from, target);
             _abiMap.Migrate<Address, byte[]>(from, target);
-
-            var unclaimed = Runtime.CallNativeContext(NativeContractKind.Stake, nameof(StakeContract.GetUnclaimed), from).AsNumber();
-            if (unclaimed > 0)
-            {
-                Runtime.CallNativeContext(NativeContractKind.Stake, nameof(StakeContract.Claim), from, from);
-            }
 
             var stake = Runtime.CallNativeContext(NativeContractKind.Stake, nameof(StakeContract.GetStake), from).AsNumber();
             if (stake > 0)
