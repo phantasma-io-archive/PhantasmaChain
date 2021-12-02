0a5362c362de8c7b584599f46188fa267fccb08a
João Novo <34069419+TeknoPT@users.noreply.github.com>
2021-12-02 16:24:37 +0000

    Tests and Encryption Utils
    [Added] SignData Tests
    [Added] Storage Test
    [Added] Encryption Utils and SignData

---
+++ Phantasma.Simulator/LinkSimulator.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using Phantasma.Blockchain;
+using Phantasma.Core.Utils;
+using Phantasma.Cryptography;
+using Phantasma.Domain;
+using Phantasma.Numerics;
+
+namespace Phantasma.Simulator
+{
+    public class LinkSimulator : WalletLink
+    {
+        [Flags]
+        public enum PlatformKind
+        {
+            None = 0x0,
+            Phantasma = 0x1,
+            Neo = 0x2,
+            Ethereum = 0x4,
+            BSC = 0x8,
+        }
+
+        private Nexus _nexus;
+        private string _name;
+        private MyAccount _account;
+        private int LinkProtocol = 2;
+
+        public LinkSimulator(Nexus Nexus, string name, MyAccount account)
+        {
+            this._nexus = Nexus;
+            this._name = name;
+            this._account = account;
+        }
+
+        public override string Nexus => _name;
+
+        public override string Name => _nexus.Name;
+
+        protected override WalletStatus Status => WalletStatus.Ready;
+
+        private PlatformKind RequestPlatform(string platform)
+        {
+            PlatformKind targetPlatform;
+
+            if (!Enum.TryParse<PlatformKind>(platform, true, out targetPlatform))
+            {
+                return PlatformKind.None;
+            }
+
+            if (!_account.CurrentPlatform.HasFlag(targetPlatform))
+            {
+                return PlatformKind.None;
+            }
+
+            if (_account.CurrentPlatform != targetPlatform)
+            {
+                _account.CurrentPlatform = targetPlatform;
+            }
+
+            return targetPlatform;
+        }
+
+        protected override void Authorize(string dapp, string token, int version, Action<bool, string> callback)
+        {            
+            if (version > LinkProtocol)
+            {
+                callback(false, "unknown Phantasma Link version " + version);
+                return;
+            }
+            
+            if (_account.CurrentPlatform != PlatformKind.Phantasma)
+                _account.CurrentPlatform = PlatformKind.Phantasma;
+            
+            var state = Status;
+            if (state == null)
+            {
+                callback(false, "not logged in");
+                return;
+            }
+
+            var result = true;
+                
+            //state.RegisterDappToken(dapp, token);
+            callback(result, result ? null : "rejected");
+        }
+
+        protected override void GetAccount(string platform, Action<Account, string> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InvokeScript(string chain, byte[] script, int id, Action<byte[], string> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SignData(string platform, SignatureKind kind, byte[] data, int id, Action<string, string, string> callback)
+        {
+            var targetPlatform = RequestPlatform(platform);
+            if (targetPlatform == PlatformKind.None)
+            {
+                callback(null, null, "Unsupported platform: " + platform);
+                return;
+            }
+
+            var state = Status;
+            if (state == null)
+            {
+                callback(null, null, "not logged in");
+                return;
+            }
+
+            if (kind != SignatureKind.Ed25519)
+            {
+                callback(null, null, kind + " signatures unsupported");
+                return;
+            }
+
+            var account = _account;
+
+            var description = System.Text.Encoding.UTF8.GetString(data);
+
+            var phantasmaKeys = account.keys;
+
+            var randomValue = new Random().Next(0, int.MaxValue);
+            var randomBytes = BitConverter.GetBytes(randomValue);
+
+            var msg = ByteArrayUtils.ConcatBytes(randomBytes, data);
+            var signature = phantasmaKeys.Sign(msg);
+
+            byte[] sigBytes = null;
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.WriteSignature(signature);
+                }
+
+                sigBytes = stream.ToArray();
+            }
+
+            var hexSig = Base16.Encode(sigBytes);
+            var hexRand = Base16.Encode(randomBytes);
+
+            callback(hexSig, hexRand, null);
+        }
+
+
+        public void forceSignData(string platform, SignatureKind kind, byte[] data, int id, Action<string, string, string> callback)
+        {
+            SignData(platform, kind, data, id, callback);
+        }
+
+        protected override void SignTransaction(string platform, SignatureKind kind, string chain, byte[] script, byte[] payload, int id, Action<Hash, string> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteArchive(Hash hash, int blockIndex, byte[] data, Action<bool, string> callback)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public class MyAccount
+        {
+            public MyAccount Instance { get; private set; }
+            public PhantasmaKeys keys;
+            public PlatformKind CurrentPlatform;
+            public string token;
+
+            public MyAccount(PhantasmaKeys keys, PlatformKind platform)
+            {
+                Instance = this;
+                this.keys = keys;
+                CurrentPlatform = platform;
+            }
+        }
+    }
+}
---
+++ Phantasma.Tests/SignDataTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phantasma.Blockchain;
+using Phantasma.Cryptography;
+using Phantasma.Cryptography.Encryption;
+using Phantasma.Numerics;
+using Phantasma.Simulator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Phantasma.Simulator.LinkSimulator;
+
+namespace Phantasma.Tests
+{
+    [TestClass]
+    public class SignDataTests
+    {
+        [TestMethod]
+        public void SignWithPhantasma()
+        {
+            // setup Nexus
+            var owner = PhantasmaKeys.Generate();
+            var nexus = new Nexus("simnet", null, null);
+            nexus.SetOracleReader(new OracleSimulator(nexus));
+            var simulator = new NexusSimulator(nexus, owner, 1234);
+
+            // Setup Users
+            var testUser1 = PhantasmaKeys.Generate();
+            var account1 = new MyAccount(testUser1, PlatformKind.Phantasma);
+            LinkSimulator link1 = new LinkSimulator(nexus, "Ac1", account1);
+
+            var testUser2 = PhantasmaKeys.Generate();
+            var account2 = new MyAccount(testUser2, PlatformKind.Ethereum);
+            LinkSimulator link2 = new LinkSimulator(nexus, "Ac2", account1);
+
+            var signature = "Ed25519";
+            var platform = "phantasma";
+
+            // Encode Data
+            var rawData = "SignWithPhantasma";
+            var encodedData = Base16.Encode(Encoding.ASCII.GetBytes(rawData));
+
+            Assert.IsTrue(rawData.Equals(Encoding.ASCII.GetString(Base16.Decode(encodedData))));
+
+            // Make a sign Data Call
+            link1.forceSignData(platform, SignatureKind.Ed25519, Encoding.ASCII.GetBytes(encodedData), 0, (signed, random, error) =>
+            {
+                Assert.IsTrue(random != encodedData);
+                Assert.IsTrue(signed != encodedData);
+                Console.WriteLine($"Error:{error}");
+                var result = EncryptionUtils.ValidateSignedData(testUser1.Address.Text, signed, random, encodedData);
+                Assert.IsTrue(result, "Not Valid");
+            });
+
+            // Make a sign Data Call
+            link2.forceSignData(platform, SignatureKind.Ed25519, Encoding.ASCII.GetBytes(encodedData), 1, (signed, random, error) =>
+            {
+                Assert.IsTrue(random != encodedData);
+                Assert.IsTrue(signed != encodedData);
+                Console.WriteLine($"Error:{error}");
+                var result = EncryptionUtils.ValidateSignedData(testUser2.Address.Text, signed, random, encodedData);
+                Assert.IsFalse(result, "Valid, but shouldn't be.");
+            });
+        }
+
+        [TestMethod]
+        public void SignWithEthereum()
+        {
+            // setup Nexus
+            var owner = PhantasmaKeys.Generate();
+            var nexus = new Nexus("simnet", null, null);
+            nexus.SetOracleReader(new OracleSimulator(nexus));
+            var simulator = new NexusSimulator(nexus, owner, 1234);
+
+            // Setup Users
+            var testUser1 = PhantasmaKeys.Generate();
+            var account1 = new MyAccount(testUser1, PlatformKind.Ethereum);
+            LinkSimulator link1 = new LinkSimulator(nexus, "Ac1", account1);
+
+            var testUser2 = PhantasmaKeys.Generate();
+            var account2 = new MyAccount(testUser2, PlatformKind.Phantasma);
+            LinkSimulator link2 = new LinkSimulator(nexus, "Ac2", account1);
+
+            var signature = "Ed25519";
+            var platform = "ethereum";
+
+            // Encode Data
+            var rawData = "SignWithEthereum";
+            var encodedData = Base16.Encode(Encoding.ASCII.GetBytes(rawData));
+
+            Assert.IsTrue(rawData.Equals(Encoding.ASCII.GetString(Base16.Decode(encodedData))));
+
+            // Make a sign Data Call
+            link1.forceSignData(platform, SignatureKind.Ed25519, Encoding.ASCII.GetBytes(encodedData), 0, (signed, random, error) =>
+            {
+                Assert.IsTrue(random != encodedData);
+                Assert.IsTrue(signed != encodedData);
+                Console.WriteLine($"Error:{error}");
+                var result = EncryptionUtils.ValidateSignedData(testUser1.Address.Text, signed, random, encodedData);
+                Assert.IsTrue(result, "Not Valid");
+            });
+
+            // Make a sign Data Call
+            link2.forceSignData(platform, SignatureKind.Ed25519, Encoding.ASCII.GetBytes(encodedData), 1, (signed, random, error) =>
+            {
+                Assert.IsTrue(random != encodedData);
+                Assert.IsTrue(signed != encodedData);
+                Console.WriteLine($"Error:{error}");
+                var result = EncryptionUtils.ValidateSignedData(testUser2.Address.Text, signed, random, encodedData);
+                Assert.IsFalse(result, "Valid, but shouldn't be.");
+            });
+        }
+
+        [TestMethod]
+        public void SignWithBNB()
+        {
+            // TODO
+        }
+
+        [TestMethod]
+        public void SignWithNEO()
+        {
+           // TODO
+        }
+    }
+}
